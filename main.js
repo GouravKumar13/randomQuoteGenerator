939c19fee2c664b37260ba8f27214b08aa08aba5
Gourav Kumar <gouravkumarat@gmail.com>
2023-01-01 02:29:00 +0530

    feats : added new features

---
+++ main.js
@@ -0,0 +1,20 @@
+const generateQuote = () => {
+    let url = "https://type.fit/api/quotes";
+    fetch(url).then( (response) => {
+    
+       let json_out = response.json()
+       return json_out
+        
+        
+    }).then( (data) => {
+
+        //this is producing random number b/w 0-1500
+        let randNum = Math.floor((Math.random() * 1643) + 1);
+        let randomQuote = data[randNum];
+    
+        
+        document.getElementById("quote").innerHTML = `${randomQuote.text}`;
+        document.getElementById("author").innerHTML = `- ${randomQuote.author ? randomQuote.author : ""}`;
+    });
+}
+
